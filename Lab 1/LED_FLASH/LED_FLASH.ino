6559c33cf7bd02f1803ff6e339e681e01d63f391
Robert Horne <r.j.horne@kent.ac.uk>
2021-01-26 13:51:03 +0000

    Update LED_FLASH.ino

--- Lab 1/LED_FLASH/LED_FLASH.ino
+++ Lab 1/LED_FLASH/LED_FLASH.ino
@@ -10,14 +10,14 @@ const int ledPin =  13;   // the number of the LED pin
 
 void setup()
 {
-  pinMode(13, OUTPUT);   // initialize digital pin 13 as an output.
+  pinMode(ledPin, OUTPUT);   // initialize digital pin 13 as an output.
 }
 
 void loop()
 {
-  digitalWrite(13, HIGH);           // turn the LED on HIGH is the voltage level)
+  digitalWrite(ledPin, HIGH);           // turn the LED on HIGH is the voltage level)
   delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(ledPin, LOW);   // turn the LED off by making the voltage LOW
   delay(1000);                      // wait for a second
 
 }
